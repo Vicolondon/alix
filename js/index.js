be2901e4639058aa909e655d7e30bc213d80cc5b
Le Montagner <victorlemonta@gmail.com>
2020-02-17 16:23:21 +0100

    Modification de la gestion aléatoire en générant les photos par le fichier js .

--- js/index.js
+++ js/index.js
@@ -2,16 +2,17 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // Composant de réfèrence
     const main_container = document.getElementsByClassName('main-container')[0];
 
-    // Mes images
-    const image_1 = document.getElementsByClassName('image-1')[0];
-    const image_2 = document.getElementsByClassName('image-2')[0];
-    const image_3 = document.getElementsByClassName('image-3')[0];
+    // Nombre max de photos a generer
+    let max_photos = 10;
 
     // Un tableau
-    const all_images = [];
-
-    // Toutes mes images dans le tableau
-    all_images.push(image_1, image_2, image_3);
+    const all_images = ['./image/1.jpg',
+    './image/2.jpg',
+    './image/3.jpg',
+    './image/4.jpg',
+    './image/5.jpg',
+    './image/6.jpg',
+    './image/7.jpg' ];
 
     // Fonction pour avoir un numéro entier aléatoire
     function randomIntFromInterval(min, max) { // min and max included 
@@ -22,17 +23,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
     main_container.addEventListener('click', (event)=>{
         // Mon numéro aléatoire
         let random_number = randomIntFromInterval(0, all_images.length-1);
-        
+
+        // Supprime le premier element du main container si le nombre dépasse max_photos
+        if( main_container.children.length > max_photos){
+            let first_child = document.getElementsByClassName('image')[0];
+            main_container.removeChild( first_child );
+        }
+        // genere du html
+        main_container.innerHTML += "<img class='image' src=" + all_images[random_number] + ">";
+
         // Je lui donne une position
-        all_images[random_number].style.top = event.clientY + 'px';
-        all_images[random_number].style.left = event.clientX + 'px';
-        all_images[random_number].style.opacity = "1";
-        // all_images.forEach(element => {
-            // if(element == all_images[random_number]){
-            //     element.style.opacity = "1";
-            // } else {
-            //     element.style.opacity = "0";
-            // }
-        // });
+        main_container.children[main_container.children.length-1].style.top = event.clientY + 'px';
+        main_container.children[main_container.children.length-1].style.left = event.clientX + 'px';
+
     });
 });
